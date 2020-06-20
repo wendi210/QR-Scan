e3ba9ea6cfa912d168c0fc900c0ee84c98fe6e89
wendi210 <nengahwendiana22@gmail.com>
2020-06-20 19:55:52 +0800

    QR-Scan

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String barcode = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('QR Code Scanner'),
+          backgroundColor: Colors.black,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Scan QR Code'),
+                onPressed: () async {
+                  try {
+                    String barcode = await BarcodeScanner.scan();
+                    setState(() {
+                      this.barcode = barcode;
+                    });
+                  } on PlatformException catch (error) {
+                    if (error.code == BarcodeScanner.CameraAccessDenied) {
+                      setState(() {
+                        this.barcode = 'Izin kamera tidak diizinkan oleh si pengguna';
+                      });
+                    } else {
+                      setState(() {
+                        this.barcode = 'Error: $error';
+                      });
+                    }
+                  }
+                },
+              ),
+              Text(
+                'Result: $barcode',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
